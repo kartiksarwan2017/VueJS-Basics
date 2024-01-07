f2a4ad0b047fd8dc94cb4e15df1beae08e9146df
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-01-07 16:02:31 +0530

    adding BarPercentage to QuestionHeader

--- QuizzesApplication/QuizApp/src/components/QuizHeader.vue
+++ QuizzesApplication/QuizApp/src/components/QuizHeader.vue
@@ -1,40 +1,42 @@
 <script setup>
-  import { defineProps } from 'vue';
+import { defineProps } from 'vue';
 
-  const {questionStatus} = defineProps(["questionStatus"]);
+const { 
+    questionStatus, 
+    barPercentage 
+    } = defineProps([
+        "questionStatus", 
+        "barPercentage"
+    ]);
 </script>
 
 <template>
     <header>
         <h4>Question {{ questionStatus }}</h4>
         <div class="bar">
-            <div class="completion">
-
-            </div>
+            <div class="completion" :style="{width: barPercentage}"></div>
         </div>
     </header>
 </template>
 
 <style scoped>
-
 header {
     margin-top: 20px;
-   }
+}
 
-   header h4 {
+header h4 {
     font-size: 30px;
-   }
+}
 
-   .bar {
+.bar {
     width: 300px;
     height: 50px;
     border: 3px solid bisque;
-   }
+}
 
-   .completion {
+.completion {
     height: 100%;
     width: 0%;
     background-color: bisque;
-   }
-  
+}
 </style>
