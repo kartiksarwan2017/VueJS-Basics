8723ea839c5527e1f0398fbd0dbbf9f68dc05246
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-02-04 15:18:12 +0530

    Fetching Data from API and using Pagination

--- API_Request_To_Fetch_Data/showsapp/src/components/BreakingBadCards.vue
+++ API_Request_To_Fetch_Data/showsapp/src/components/BreakingBadCards.vue
@@ -46,31 +46,105 @@ import axios from "axios";
     </div>
 </template> -->
 
-<script setup>
+<script>
 import axios from "axios";
-import { ref, watch } from "vue";
+import { ref, watch, onMounted } from "vue";
+import Card from "./Card.vue";
+import md5 from 'md5';
 
+export default {
+    setup() {
     const characters = ref(null);
     const page = ref(0);
+    const publicKey= ref("079e684d61a8f93d1cf7dcce70d45ef5");
+    const  privateKey= ref("3290aa55419da747278751418487809eb0124144");
+    const  timestamp= Math.floor(Date.now() / 1000).toString();
+    const hash= ref("");
+    const apiUrl = ref("https://gateway.marvel.com/v1/public/characters");
 
-    const response  = await axios.get(`https://pokeapi.co/api/v2/ability/?limit=8`);
-    characters.value = response.data.results;
+    const generateHash = () => {
+        const hashInput = timestamp + privateKey.value + publicKey.value;
+        hash.value = md5(hashInput);
+    }
+
+    const makeApiRequest = async () => {
+        const fullApiUrl = `${apiUrl.value}?limit=8&apikey=${publicKey.value}&ts=${timestamp}&hash=${hash.value}`;
+
+        const response  = await axios.get(`${fullApiUrl}`);
+        characters.value = response.data.data.results;
+    }
+
+    onMounted(() => {
+        generateHash();
+        makeApiRequest();
+    });
 
     watch(page, async () => {
-        const res = await axios.get(`https://pokeapi.co/api/v2/ability/?limit=8&offset=${page.value*8}`);
-        characters.value = res.data.results;
-        console.log(res);
-    });
+    const res = await axios.get(`${apiUrl.value}?limit=8&offset=${page.value*8}&apikey=${publicKey.value}&ts=${timestamp}&hash=${hash.value}`);
+    characters.value = res.data.data.results;
+});
+
+return {
+    characters,
+    page
+}
+}
+}
 
 </script>
 
 <template>
-    <div>
-        <h1>Breaking Bad Cards</h1>
-        <h1>{{ characters }}</h1>
-        <div>
-            <button @click="page = page + 1">Next</button>
-            <button @click="page = page - 1">Back</button>
+    <!-- <div class="container">
+        <div class="cards">
+            <Card />
         </div>
+    </div> -->
+    <div>
+        {{ characters }}
+
+        <button @click="page = page + 1">next</button>
+        <button @click="page = page - 1">prev</button>
     </div>
 </template>
+
+<style scoped>
+.container {
+    background-color: rgb(27, 26, 26);
+    padding: 30px
+}
+.cards {
+    max-width: 1000px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    height: 700px
+}
+.cards h3 {
+    font-weight: bold;
+}
+.cards p {
+    font-size: 10px;
+}
+.jobs {
+    display: flex;
+    flex-wrap: wrap;
+}
+.button-container {
+    display: flex;
+    justify-content: center;
+    padding-top: 30px
+}
+.button-container button {
+    border: none;
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    margin: 0 5px;
+    cursor: pointer;
+}
+.spinner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ API_Request_To_Fetch_Data/showsapp/src/components/Card.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <div></div>
+    <!-- <NCard title="Card with Cover">
+        <template #cover>
+          <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
+        </template>
+        Card Content
+    </NCard> -->
+</template>
+
+<style scoped>
+.n-card {
+    width: 200px;
+    margin:10px 20px;
+    height: 400px;
+
+}
+.n-card img {
+    height: 250px;
+}
+
+p {
+    font-size: 10px;
+}
+
+.jobs {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
