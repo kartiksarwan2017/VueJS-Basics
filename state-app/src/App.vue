892a458180ce25fb56c8aca984d46df189cbeac3
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-02-10 13:02:13 +0530

    Introduction to State Management (Building Color Generator Application)

---
+++ state-app/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+   import {ref} from "vue";
+
+   const red = ref(0);
+   const blue = ref(0);
+   const green = ref(0);
+
+</script>
+
+<template>
+  <main :style="{backgroundColor: `rgb(${red}, ${green}, ${blue})`}">
+    <div>
+      <input type="number" v-model="red" placeholder="red"/>
+      <input type="number" v-model="blue"  placeholder="blue"/>
+      <input type="number" v-model="green"  placeholder="green"/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
