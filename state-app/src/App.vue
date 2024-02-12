815bc570c31d88bc15aad1e06547c31a2d036d0a
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-02-12 16:06:39 +0530

    A lof of Nested Components

--- state-app/src/App.vue
+++ state-app/src/App.vue
@@ -1,48 +1,10 @@
 <script setup>
-   import { ref, reactive } from "vue";
-
-  //  const red = ref(0);
-  //  const blue = ref(0);
-  //  const green = ref(0);
-
-  // const color = ref({
-  //   red: 0,
-  //   green: 0,
-  //   blue: 0
-  // });
- 
-  // const selecRandomColor = () => {
-  //   // color.value.red = Math.floor(Math.random()*255),
-  //   // color.value.blue = Math.floor(Math.random()*255),
-  //   // color.value.green = Math.floor(Math.random()*255)
-
-  //   color.value = true
-  // }
-
-  /* Defining State using reactive */
-  const color = reactive({
-    red: 0,
-    green: 0,
-    blue: 0
-  });
-
-  const selecRandomColor = () => {
-    color.red = Math.floor(Math.random()*255),
-    color.blue = Math.floor(Math.random()*255),
-    color.green = Math.floor(Math.random()*255)
-
-    // color = {yellow: 4}
-  }
+import Parent from "./components/Parent.vue";
 </script>
 
 <template>
-  <main :style="{backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`}">
-    <div>
-      <input type="number" v-model="color.red" placeholder="red"/>
-      <input type="number" v-model="color.blue"  placeholder="blue"/>
-      <input type="number" v-model="color.green"  placeholder="green"/>
-      <button @click="selecRandomColor">Select Random Color</button>
-    </div>
+  <main>
+    <Parent />
   </main>
 </template>
 
