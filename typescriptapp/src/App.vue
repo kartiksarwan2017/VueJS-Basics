04e8f4bbc3ab737fa56c4133d2bb53a8517194d9
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-02-21 09:30:38 +0530

    Vuejs and Typescript

---
+++ typescriptapp/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  const number = ref("");
+</script>
+
+<template>
+  <main>
+    <h1>Guess My Favourite Number</h1>
+    <input type="text" v-model="number" />
+    <h1 v-if="number === '27'">YOU GUESS IT</h1>
+    <!-- {{ typeof(number) }}
+    {{ typeof(27) }} -->
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ vue-with-vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+  <ComponentOne />
+  </div>
+</template>
+
+<script>
+import ComponentOne from "./components/Component.vue";
+export default {
+  name: 'App',
+  components: {
+    ComponentOne
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
